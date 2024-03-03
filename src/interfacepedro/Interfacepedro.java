ab9e9bd9edbc645730b206f55b2910401151801b
DanielIbarra90 <162011388+DanielIbarra90@users.noreply.github.com>
2024-03-03 10:08:01 -0600

    practica

---
+++ src/interfacepedro/Interfacepedro.java
@@ -0,0 +1,15 @@
+
+package interfacepedro;
+
+
+public class Interfacepedro {
+
+    
+    public static void main(String[] args) {
+        RFC dani =new RFC();
+        dani.setVisible(true);
+        dani.setLocationRelativeTo(null);
+    
+    }
+    
+}
